172e0ef2921eed98964b11b35c8378546c5acc5e
shaack <shaack@gmail.com>
2021-12-10 18:12:21 +0100

    ready for production beta test

--- src/stakelbase/Book.js
+++ src/stakelbase/Book.js
@@ -1,25 +1,16 @@
-//
+
 // Code dealing with Polyglot opening books
-//
-
-
-//
 // Representation of a single entry in a Polyglot opening book
-//
+
 class BookEntry {
-    //
+
     // Build one BookEntry from the binary data slurped from
     // a polyglot opening book starting at byte-offset 'ofs'
-    //
     constructor(bookdata, ofs) {
-        //
         // Remember where we started from
-        //
         // noinspection JSUnusedGlobalSymbols
         this.ofs = ofs
-        //
         // Convert key to BigInt (64-bit) value
-        //
         let key = BigInt(0)
         let i
         let byt
@@ -83,30 +74,24 @@ class BookEntry {
     // is encoded in current move.
     //
     isOOW() {
-        const m = this.raw_move
-        return (m === 0x0107)
+        return this.raw_move === 0x0107
     }
 
     isOOB() {
-        const m = this.raw_move
-        return (m === 0x0f3f)
+        return this.raw_move === 0x0f3f
     }
 
     isOOOW() {
-        const m = this.raw_move
-        return (m === 0x0100)
+        return this.raw_move === 0x0100
     }
 
     isOOOB() {
-        const m = this.raw_move
-        return (m === 0x0f38)
+        return this.raw_move === 0x0f38
     }
 
 }
 
-//
 // representation of a Polyglot binary opening book
-//
 export class Book {
     constructor(bookdata) {
         this.bookdata = bookdata
@@ -143,12 +128,10 @@ export class Book {
         return e
     }
 
-    //
     // Retrieve the index of the first occurrence of the specified hash,
     // or -1 if not found.
     //
     // weed = BigInt (64-bit integer) with the hash to locate
-    //
     find_first_hash(weed) {
         if (this.first === null || this.last === null) {
             return -1
@@ -163,34 +146,26 @@ export class Book {
         let i1 = (this.bookdata.length / 16) - 1
         let i = i1
         let ky = 0n	// need a BigInt here
-        //
         // If the last entry matches the searched hash value,
         // we can skip the binary search and go straight to
         // the part where we move up in the list of same hash
         // values until we find the first in the list.
-        //
         if (weed !== this.last) {
-            //
             // Stop looping if only 2 elements are in the inspection range:
             // both endpoints will have been compared to the wanted hash value
             // and both endpoints of the range will have a different hash. So,
             // if there are only two left, and both have previously been confirmed
             // to be different from the wanted hash, that means that the hash is
             // not in the table and we can safely bail out of the loop.
-            //
             while (i1 - i0 > 1) {
                 i = Math.floor((i0 + i1) / 2)	// this is javascript for you: no int arithmetic
                 const e = this.get_entry(i)
                 ky = e.get_key()
                 if (ky === weed) {
-                    //
                     // Found an entry with the wanted hash: stop looping
-                    //
                     break
                 }
-                //
                 // Not found, so continue with half the current range
-                //
                 if (ky < weed) {
                     i0 = i
                 } else {
@@ -198,15 +173,11 @@ export class Book {
                 }
             }
         }
-        //
         // No need to go up in the table if not found
-        //
         if (ky !== weed) {
             return -1
         }
-        //
         // Move up in table until different key found
-        //
         while (i > 0) {
             if (this.get_entry(i - 1).get_key() === weed) {
                 i = i - 1
@@ -217,9 +188,7 @@ export class Book {
         return i
     }
 
-    //
     // Get all entries for given FEN position
-    //
     get_all_moves(weed) {
         let i = this.find_first_hash(weed)
         if (i < 0) {
